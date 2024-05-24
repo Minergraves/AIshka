a1c7efb5d06fff37577cae5ffb804a2797281ee3
Minergraves <voffka1601@gmail.com>
2024-05-24 15:52:44 +0300

    bots (friend and fruits)

--- bot best friend ).py
+++ bot best friend ).py
@@ -5,18 +5,22 @@ from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
-TOKEN = '6974689443:AAEpC2IWegBtPlDfuPMvmziEcqML2I2Jm9o'
+TOKEN = '6554705021:AAFDOrIAl1559jZ3lKmvnRVdbqquZr2Jcnc'
 #диспетчер
 dp = Dispatcher()
 #обработка команды старт
 @dp.message(CommandStart())
 async def echo(message: Message):
-  await message.reply('Привет! Я бот, распознающий овощи и фрукты')
+  await message.reply('Привет! Я ботик, который всегда может выслушать и дать совет. Расскажи мне о своей проблеме и я дам тебе решение :3')
+
+@dp.message(F.text)
+async def handle_message(message: Message):
+  await message.reply('Иди нахуй')
 
 # обработка команды help
-@dp.message(F.text, Command("test"))
+@dp.message(F.text, Command("/help"))
 async def echo(message: Message):
-  await message.reply('Просто отправьте мне изображение, которое содержит овощ или фрукт')
+  await message.reply('Просто скажи мне свою проблему и я дам решение')
 
 async def main() -> None:
   # Initialize Bot instance with default bot properties which will be passed to all API calls
